ad68c89e1ca8ab4947cfb92a22e45e982a75d11e
unknown <dy.yev.solovyov@gmail.com>
2023-04-08 07:03:40 +0300

    changed cv link

--- src/components/cv/index.tsx
+++ src/components/cv/index.tsx
@@ -4,7 +4,7 @@ import { IconSelectorComponent } from "../icon-selector";
 export const CvComponent: FC = (): JSX.Element => {
 	return (
 		<a
-			href="https://drive.google.com/file/d/1-dtk-uwG8m5ZpD_QwSIDepcpkDSwK1yx/view?usp=sharing"
+			href="https://drive.google.com/file/d/12BLdezCtlUsP1veKe1OYcKaei8ntrjjs/view?usp=sharing"
 			target="_blank"
 			rel="noreferrer"
 			className="w-[60px] h-[60px] flex items-center justify-center text-[#d4bc22] border-[#00cea8] border-[3px] rounded-full border cursor-pointer"
